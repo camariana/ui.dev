4b1615adbc51f14c977ef8a2c5444c2651766335
camariana <camariana40@outlook.com>
2021-05-08 12:54:01 +0000

    show or hide solution

--- show-hide/src/App.js
+++ show-hide/src/App.js
@@ -9,18 +9,31 @@ import React, { useState } from 'react';
 
 
 const App = ({ posts }) => {
-  const [post, setPost] = useState([ ...posts ])
-  const [togglePost, setTogglePost] = useState(false);
-
-  console.log(post);
-
-  const show = () => setTogglePost('show');
-  const hide = () => setTogglePost('hide');
+  const [openIndex, setOpenIndex] = useState(0); // the first one in the array is open by default
 
   return (
     <ul>
       {/* {JSON.stringify(posts, null, 2)} */}
 
+      {posts.map(({ id, img, text }, index) => {
+        const isOpen = openIndex === index;
+
+        const textToShow = isOpen ? text : text.substring(0, 100) + '...';
+
+        return (
+          <li key={id} style={{border: isOpen ? '1px solid black' : 'none'}}>
+              <img src={img} alt="" />
+              <p>
+                {textToShow} 
+              </p>
+              {!isOpen && (
+                <button onClick={() => setOpenIndex(index)}>
+                  Open
+                </button>
+              )}
+          </li>
+        )
+      })}
       
     </ul>
   )
